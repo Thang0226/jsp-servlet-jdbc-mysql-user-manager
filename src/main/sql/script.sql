6cdea591f6f1b1180918202b6dc7812138dc4b01
Thang0226 <thang.nd0226@gmail.com>
2024-12-03 14:02:49 +0700

    add sql insert_user procedure

--- src/main/sql/script.sql
+++ src/main/sql/script.sql
@@ -29,3 +29,36 @@ BEGIN
  where users.id = user_id;
  END$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE insert_user(
+	IN user_name varchar(50),
+    IN user_email varchar(50),
+    IN user_country varchar(50)
+)
+BEGIN
+	INSERT INTO users(name, email, country) VALUES(user_name, user_email, user_country);
+ END$$
+DELIMITER ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
